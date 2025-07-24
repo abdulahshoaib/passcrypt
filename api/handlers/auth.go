7ec8da8ee2558d01fb453aaf92d9549bf5ad07c9
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-07-24 17:26:32 +0500

    feat: signup and delete account handler

---
+++ api/handlers/auth.go
@@ -0,0 +1,18 @@
+package handlers
+
+import ()
+
+// Register to the service
+func RegisterHandler() {
+	// This should ask user for his username
+	// a passcode for that username
+
+	// server creates a db entry into against username
+	// db also keeps a hashed version of the passcode
+	// The passcode will be the valut name
+}
+
+// should delete the user data if sent with correct passcode
+func DeleteHandler()  {
+	// Delete query onto the database to delte the user's data
+}
